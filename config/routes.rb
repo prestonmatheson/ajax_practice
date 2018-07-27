325fc636caf503c1b7cee6f544c2dbcb7d5d72d3
Preston Matheson <pmphoto123@gmail.com>
2018-07-27 15:24:02 -0700

    I generated a Post scaffold and installed jquery to play around with ajax calls

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
+  root to: 'posts#index', as: 'root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
